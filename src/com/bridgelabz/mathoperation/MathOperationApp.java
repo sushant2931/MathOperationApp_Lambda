34da9e8cabb49bfde63570dfe328756b25dc62d4
sushant2931 <sushantlad8888@gmail.com>
2021-09-22 22:19:16 +0530

    Added : Welcome In Math Operation App Using Lambda Function

---
+++ src/com/bridgelabz/mathoperation/MathOperationApp.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.mathoperation;
+
+public class MathOperationApp {
+    public static void main(String[] args) {
+        System.out.println("....... Welcome On Math Operation App Using Lambda Function ....");
+    }
+}
