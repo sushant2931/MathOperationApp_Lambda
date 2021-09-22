c82ff04fb262f8f94e991b9d3c2f7e14b9877a8c
sushant2931 <sushantlad8888@gmail.com>
2021-09-22 22:31:16 +0530

    ADDED : Passing Lambda Fuunction To Print Result

--- src/com/bridgelabz/mathoperation/MathOperationApp.java
+++ src/com/bridgelabz/mathoperation/MathOperationApp.java
@@ -3,18 +3,25 @@ package com.bridgelabz.mathoperation;
 @FunctionalInterface
 interface MathFunctionIF{
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function, MathFunctionIF functionObject) {
+        System.out.println("RESULT OF " +function+" is "+functionObject.calculate(a,b));
+    }
 }
 public class MathOperationApp {
     public static void main(String[] args) {
         System.out.println("....... Welcome On Math Operation App Using Lambda Function ....");
+
         MathFunctionIF add = Integer::sum;
         MathFunctionIF subtract = (int a, int b) -> a-b;
         MathFunctionIF multiply = (int a, int b) -> a*b;
-        MathFunctionIF divide = (int a, int b) -> a/b;
+        MathFunctionIF divide = (int a, int b) -> {
+            if(b==0)return 0;
+            return a/b;
+        };
 
-        System.out.println("ADD: "+add.calculate(6, 8));
-        System.out.println("SUBTRACT: "+subtract.calculate(6, 8));
-        System.out.println("MULTIPLY: "+multiply.calculate(6, 8));
-        System.out.println("DIVIDE: "+divide.calculate(6, 8));
+        MathFunctionIF.printResult(6, 5, "ADDITION", add);
+        MathFunctionIF.printResult(6, 5, "SUBTRACT", subtract);
+        MathFunctionIF.printResult(6, 5, "MULTIPLY", multiply);
+        MathFunctionIF.printResult(6, 0, "DIVIDE", divide);
     }
 }
